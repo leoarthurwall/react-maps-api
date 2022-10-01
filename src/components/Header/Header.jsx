5fb09980a3618cad0866b9c1a99c4b17be1e1954
leoarthurwall <leowall93@gmail.com>
2022-10-01 23:24:31 +0100

    added a menu icon and an on click function so that when it's clicked, it displays a cross instead·

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
-import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import './Header.css'
+import { useState } from "react";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import "./Header.css";
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div className="header-container">
-      <h1 className="header-title">Travel Advisor</h1>
+      <div className="menu-title-container">
+       {isClicked ? ( <XMarkIcon onClick={handleClick} className="menu-icon" />) : ( <Bars3Icon onClick={handleClick} className="menu-icon" />)}
+       
+        <h1 className="header-title">Travel Advisor</h1>
+      </div>
       <div className="search-container">
         <h3 className="search-title">Expore new Places</h3>
         <form className="input-form">
           <input className="input-box" type="input" placeholder="Search" />
-          <MagnifyingGlassIcon className="search-icon"/>
+          <MagnifyingGlassIcon className="search-icon" />
         </form>
       </div>
     </div>
