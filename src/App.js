020d05a9a04f0a3d8ffd467d22e6ee9f31df869f
leoarthurwall <leowall93@gmail.com>
2022-10-01 18:36:14 +0100

    created ui for header component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-
+import Header from "./components/Header/Header";
 
 function App() {
-
-
-
   return (
     <>
       <div className="App">
-       <h1> Hello World</h1>
+        <Header />
       </div>
     </>
   );
 }
-
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import './Header.css'
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header-container">
+      <h1 className="header-title">Travel Advisor</h1>
+      <div className="search-container">
+        <h3 className="search-title">Expore new Places</h3>
+        <form className="input-form">
+          <input className="input-box" type="input" placeholder="search for places" />
+          <MagnifyingGlassIcon className="search-icon"/>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
