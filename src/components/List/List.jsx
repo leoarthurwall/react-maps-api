42fc431da7f9e9af141dc2539f831c54e4956584
leoarthurwall <leowall93@gmail.com>
2022-10-02 11:38:01 +0100

    created a dropdown for type, give it a placeholder rather than label. now looking to style the select tag

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,8 +4,18 @@ import "./List.css";
 const List = () => {
   return (
     <>
-      <div className="list-container"> 
-      <h4 className="list-header">Resturants, Hotels & Attractions around you</h4>
+      <div className="list-container">
+        <h4 className="list-header">
+          Resturants, Hotels & Attractions around you
+        </h4>
+        <form className="type-form">
+          <select name="type" className="type-dropdown">
+            <option value="hotels" disabled selected hidden>Type</option>
+            <option value="hotels">Hotels</option>
+            <option value="resturants">Resturants</option>
+            <option value="attractions">Attractions</option>
+          </select>
+        </form>
       </div>
       <div className="list-overlay"></div>
     </>
