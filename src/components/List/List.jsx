b7123d9da526df9948daf864c9b56aa33390424f
leoarthurwall <leowall93@gmail.com>
2022-10-02 11:11:58 +0100

    added header to the list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,7 +4,9 @@ import "./List.css";
 const List = () => {
   return (
     <>
-      <div className="list-container"> </div>
+      <div className="list-container"> 
+      <h4 className="list-header">Resturants, Hotels & Attractions around you</h4>
+      </div>
       <div className="list-overlay"></div>
     </>
   );
