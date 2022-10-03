727dc2c214ec344481d07830c27d148ea6d1aa77
leoarthurwall <leowall93@gmail.com>
2022-10-03 12:11:57 +0100

    successfully passed props into the dropdown component making it reusable for both the type dropdown and the rating dropdown

--- src/components/List/Dropdown/Dropdown.jsx
+++ src/components/List/Dropdown/Dropdown.jsx
@@ -1,36 +1,38 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
-import './Dropdown.css'
+import "./Dropdown.css";
 
-const Dropdown = () => {
-    const [isDropdownClicked, setIsDropdownClicked] = useState(false);
+const Dropdown = ({ title, listItems }) => {
+  const [isDropdownClicked, setIsDropdownClicked] = useState(false);
+
+  console.log(listItems);
 
   const handleDropdown = () => {
     setIsDropdownClicked(!isDropdownClicked);
   };
   return (
     <form className="type-form">
-          <div className="select-container-wrapper">
-            <div className="select-container" onClick={handleDropdown}>
-              <p className="select-header-text">Type</p>
-              {isDropdownClicked ? (
-                <ChevronUpIcon className="select-arrow" />
-              ) : (
-                <ChevronDownIcon className="select-arrow" />
-              )}
-            </div>
-            {isDropdownClicked && (
-              <div className="select-list-container">
-                <ul className="select-list">
-                  <ol className="select-list-item">Resturants</ol>
-                  <ol className="select-list-item">Hotels</ol>
-                  <ol className="select-list-item">Amusements</ol>
-                </ul>
-              </div>
-            )}
+      <div className="select-container-wrapper">
+        <div className="select-container" onClick={handleDropdown}>
+          <p className="select-header-text">{title}</p>
+          {isDropdownClicked ? (
+            <ChevronUpIcon className="select-arrow" />
+          ) : (
+            <ChevronDownIcon className="select-arrow" />
+          )}
+        </div>
+        {isDropdownClicked && (
+          <div className="select-list-container">
+            <ul className="select-list">
+              {listItems.map((listItem, index) => {
+                return <ol className="select-list-item">{listItem}</ol>;
+              })}
+            </ul>
           </div>
-        </form>
-  )
-}
+        )}
+      </div>
+    </form>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,6 +2,8 @@ import Dropdown from "./Dropdown/Dropdown";
 import "./List.css";
 
 const List = () => {
+  const typeList = ["Hotels", "Resturants", "Attractions"];
+  const ratingList = ["3 Stars or less", "3.5 Stars", "4 Stars", "4.5 Stars"];
   return (
     <>
       <div className="list-container">
@@ -9,8 +11,8 @@ const List = () => {
           Resturants, Hotels & Attractions around you
         </h4>
         <div className="dropdown-container">
-          <Dropdown />
-          <Dropdown />
+          <Dropdown title={"Type"} listItems={typeList} />
+          <Dropdown title={"Rating"} listItems={ratingList} />
         </div>
       </div>
       <div className="list-overlay"></div>
