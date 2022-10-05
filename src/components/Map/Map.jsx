fe333bc94100ba2e111d70a1f6dbe6269986abcd
leoarthurwall <leowall93@gmail.com>
2022-10-05 19:45:28 +0200

    in the map.js, added an on change handler to the googlemapreact component, that set the Coordinates and bounds upon any change. this will populate the coordinates and bounds state

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,42 +1,36 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import './Map.css'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import "./Map.css";
 
-
-const Map = ({setCoordinates, setBounds, coordinates}) => {
-
-
-    const defaultProps = {
-        center: {
-          lat:  51.5007,
-          lng: -0.1246
-        },
-        zoom: 14
-      };
-
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const defaultProps = {
+    center: {
+      lat: 51.5007,
+      lng: -0.1246,
+    },
+    zoom: 14,
+  };
 
   return (
-    <div className='map-container'>
-       <GoogleMapReact
-       bootstrapURLKeys={{key: process.env.REACT_APP_MAP_API_KEY,}}
-       defaultCenter={defaultProps.center}
-       center={defaultProps.center}
-       defaultZoom={defaultProps.zoom}
-    //    margin={[50, 50, 50, 50]}
-    //    options={''}
-       onChange={(e) => {
-        console.log(e)
-        setCoordinates({lat: e.center.lat, lng: e.center.lng})
-       }}
-    //    onChildClick={''}
-    //    onChildMouseEnter={this.onChildMouseEnter}
-    //    onChildMouseLeave={this.onChildMouseLeave}
-
-       >
-
-       </GoogleMapReact>
+    <div className="map-container">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
+        defaultCenter={defaultProps.center}
+        center={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        //    margin={[50, 50, 50, 50]}
+        //    options={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        //    onChildClick={''}
+        //    onChildMouseEnter={this.onChildMouseEnter}
+        //    onChildMouseLeave={this.onChildMouseLeave}
+      ></GoogleMapReact>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
