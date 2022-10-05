dc922f0113bedd1ae4f88eec216ab251a0b06986
leoarthurwall <leowall93@gmail.com>
2022-10-05 18:17:34 +0200

    added a useState on the app.js for the places. which is an empty array. Then in the use effect function, setPlaces state with data

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import { useState, useEffect } from "react";
 import List from "./components/List/List";
-import { getPlacesData } from './api'
-
+import { getPlacesData } from "./api";
 
 function App() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
-  
+  const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    getPlacesData();
-  }, [])
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
   return (
     <>
       <div className="App">
-        <Header isMenuClicked={isMenuClicked} setIsMenuClicked={setIsMenuClicked} />
-        { isMenuClicked && ( <List /> )}
+        <Header
+          isMenuClicked={isMenuClicked}
+          setIsMenuClicked={setIsMenuClicked}
+        />
+        {isMenuClicked && <List />}
         {/* <Map /> */}
       </div>
     </>
