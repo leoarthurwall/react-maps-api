793b522f8a4c748500b3b05c82d8cc28786a479e
leoarthurwall <leowall93@gmail.com>
2022-10-05 20:10:00 +0200

    created another useEffect that sets the coordinates state to the user's current location. The default center and center on the GoogleMapReact is now set to the coordinates state

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
   const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
+    setCoordinates({lat: latitude, lng: longitude})
+  })
+  }, [])
+
+  useEffect(() => {
     console.log(coordinates, bounds)
 
     getPlacesData().then((data) => {
