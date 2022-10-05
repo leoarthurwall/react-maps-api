c9c2bbc54e2ebf4e75511e43ece3ce239f1e2d44
leoarthurwall <leowall93@gmail.com>
2022-10-05 17:56:10 +0200

    got axios api code from rapid api to display restrurants by screen boundries

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
+const URL =  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+const options = {
+    url: URL,
+    params: {
+      bl_latitude: '11.847676',
+      tr_latitude: '12.838442',
+      bl_longitude: '109.095887',
+      tr_longitude: '109.149359',
+    },
+    headers: {
+    
+    }
+  };
+  
+
 const getPlacesData = async () => {
   try {
-    const response = await axios.get()
+    const response = await axios.get(URL, options)
   } catch (error) {
    
   }
