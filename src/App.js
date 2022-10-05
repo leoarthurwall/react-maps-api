c539c5c61c793d9a80769ddcd98081d1dc0ba413
leoarthurwall <leowall93@gmail.com>
2022-10-05 18:07:11 +0200

    imported use effect on app.js. imported getPlacesData to app.js. wrote a useEffect function with an empty dependency array calling the get places Data function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "./components/List/List";
+import { getPlacesData } from './api'
 
 
 function App() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
+  
 
+  useEffect(() => {
+    getPlacesData();
+  }, [])
   return (
     <>
       <div className="App">
