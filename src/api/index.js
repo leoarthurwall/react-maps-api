fa8c9da4a7c83bd99bd5351ec4dbda9d9601e60b
leoarthurwall <leowall93@gmail.com>
2022-10-05 18:47:58 +0200

    api is now requesting and responding successfully to the axios call to the travel advisor api. issue was that I hadn't subscribed. figured this out by readiong the 403 error on the console

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,27 @@
 import axios from "axios";
 
-const URL =  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
 const options = {
-    url: URL,
-    params: {
-      bl_latitude: '11.847676',
-      tr_latitude: '12.838442',
-      bl_longitude: '109.095887',
-      tr_longitude: '109.149359',
-    },
-    headers: {
-      'X-RapidAPI-Key': 'e0d2821039mshc0f6204214b6301p1c640bjsn377ae7a3b52e',
-      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-    }
-  };
-  
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+ 
+  },
+};
 
-const getPlacesData = async () => {
+export const getPlacesData = async () => {
   try {
-    const {data: {data}} = await axios.get(URL, options)
+    const {
+      data: { data } } = await axios.get(URL, options);
 
-    return data
+    return data;
   } catch (error) {
-   console.log(error)
+    console.log(error);
   }
 };
