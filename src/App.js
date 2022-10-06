252e1f7c2dc765ff648973a7d397e1dce8dec536
leoarthurwall <leowall93@gmail.com>
2022-10-06 21:59:10 +0200

    removed the lat and lng from the bounds state as it's not needed. left as an empty object

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
   const [places, setPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState({ lat: 51.5072, lng: 0.1276 });
+  const [bounds, setBounds] = useState({});
   // const [bounds, setBounds] = useState(null); original state, doesn't work - possible because wifi conmnection is so slow
 
   useEffect(() => {
