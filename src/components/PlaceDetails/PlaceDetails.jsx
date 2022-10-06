fad9ca527ce7ce6be2078a46e1f93433dec7c518
leoarthurwall <leowall93@gmail.com>
2022-10-06 20:10:37 +0200

    added overflow scroll to th list card component, started creating the JSX div structure for the place details component

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "../List/List.css";
+import "./PlaceDetails.css";
 
 const PlaceDetails = ({ place }) => {
   console.log("place:", place);
   return (
     <div className="place-container">
-      <h3 className="card-header">{place.name}</h3>
-      <p>{place.address}</p>
-      <p>{place.rating} ({place.num_reviews})</p>
-      <p>{place.open_now_text}</p>
-      <p>{place.phone}</p>
-      <p>{place.price_level}</p>
-      <p>{place.web_url}</p>
-      <p>{place.website}</p>
-
-      <img className="card-image" src={place.photo ? place.photo.images.small.url : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"} alt="place" />
+      <div className="place-left">
+        <h3 className="card-header">{place.name}</h3>
+        <p>{place.address}</p>
+        <p>
+          {place.rating} ({place.num_reviews})
+        </p>
+        <p>{place.open_now_text}</p>
+        <p>{place.phone}</p>
+        <p>{place.price_level}</p>
+        <p>{place.web_url}</p>
+        <p>{place.website}</p>
+      </div>
+      <div className="place-right">
+        <img
+          className="card-image"
+          src={
+            place.photo
+              ? place.photo.images.small.url
+              : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"
+          }
+          alt="place"
+        />
+      </div>
     </div>
   );
 };
