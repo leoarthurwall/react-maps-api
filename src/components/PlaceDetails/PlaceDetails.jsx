30e9b8cb1ae293101c4e8502e521569af29b4971
leoarthurwall <leowall93@gmail.com>
2022-10-06 21:05:29 +0200

    added a tags to the resturant name, image web and tripadv tags

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -8,7 +8,9 @@ const PlaceDetails = ({ place }) => {
   return (
     <div className="place-container">
       <div className="place-left">
-        <h3 className="place-header">{place.name}</h3>
+        <a className="place-url" href={place.web_url} target="blank">
+          <h3 className="place-header">{place.name}</h3>
+        </a>
         <div className="place-rating-price-container">
           <p className="place-rating-bold">{place.rating}</p>
           <StarIcon className="place-rating-icon" />
@@ -22,19 +24,25 @@ const PlaceDetails = ({ place }) => {
 
         <p className="place-open">{place.open_now_text}</p>
         <p className="place-phone">{place.phone}</p>
-        <p className="place-web">{place.web_url}</p>
-        <p className="place-tripadv">{place.website}</p>
+        <a className="place-url" href={place.web_url} target="blank">
+          <p className="place-web">Website</p>
+        </a>
+        <a className="place-url" href={place.website} target="blank">
+        <p className="place-tripadv">Trip Advisor</p>
+      </a>
       </div>
       <div className="place-right">
-        <img
-          className="place-image"
-          src={
-            place.photo
-              ? place.photo.images.small.url
-              : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"
-          }
-          alt="place"
-        />
+        <a className="place-url" href={place.web_url} target="blank">
+          <img
+            className="place-image"
+            src={
+              place.photo
+                ? place.photo.images.small.url
+                : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"
+            }
+            alt="place"
+          />
+        </a>
       </div>
     </div>
   );
