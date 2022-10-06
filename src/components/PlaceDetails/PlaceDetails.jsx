0e0296861cd4c26cff5008778ecf9fa031f9f0d9
leoarthurwall <leowall93@gmail.com>
2022-10-06 19:44:47 +0200

    displaying all the api data that's wanted: resturant name, address, rating(no.) open now, phone number, price rance, weblink, trip advisor link, image

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import "../List/List.css";
 
-const PlaceDetails = ({place}) => {
+const PlaceDetails = ({ place }) => {
+  console.log("place:", place);
   return (
-        <h3 className="card-header">{place.name}</h3>
+    <div className="place-container">
+      <h3 className="card-header">{place.name}</h3>
+      <p>{place.address}</p>
+      <p>{place.rating} ({place.num_reviews})</p>
+      <p>{place.open_now_text}</p>
+      <p>{place.phone}</p>
+      <p>{place.price_level}</p>
+      <p>{place.web_url}</p>
+      <p>{place.website}</p>
+
+      <img className="card-image" src={place.photo ? place.photo.images.small.url : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"} alt="place" />
+    </div>
   );
 };
 
 export default PlaceDetails;
+
+/*
+Inclide in the card
+- Title DONE
+- Rating (score and number of reviews) DONE
+- Price range DONE
+- type (resturant, takeaway)
+- open/closed
+- image
+- website 
+- tripadvisor
+*/
