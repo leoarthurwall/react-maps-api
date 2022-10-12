e84ef35ca030c945d89f67f1a40b00b9d28da33e
leoarthurwall <leowall93@gmail.com>
2022-10-12 13:47:18 +0200

    added rating to the card

--- src/components/Map/MapCard/MapCard.jsx
+++ src/components/Map/MapCard/MapCard.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { MapPinIcon, StarIcon } from "@heroicons/react/24/solid";
-import './MapCard.css'
+import "./MapCard.css";
+import PlaceDetails from "../../PlaceDetails/PlaceDetails";
 
 const MapCard = ({ place }) => {
   return (
     <>
-      <h4 className="place-title-marker">{place.name}</h4>
-      <img
-        className="place-image-marker"
-        src={
-          place.photo
-            ? place.photo.images.small.url
-            : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"
-        }
-        alt="place"
-      />
-      <div className="reviews-container-marker">
-        <p className="place-rating-bold">{place.rating}</p>
-        <StarIcon className="place-rating-icon" />
-      </div>
-      {/* <MapPinIcon className="map-pin-icon"/> */}
+      {place.name ? (
+        <>
+          <h4 className="place-title-marker">{place.name}</h4>
+          <img
+            className="place-image-marker"
+            src={
+              place.photo
+                ? place.photo.images.small.url
+                : "http://www.resto.be/across/resources/static/site/images/placeholder-detail-resto.jpg"
+            }
+            alt="place"
+          />
+          <div className="reviews-container-marker">
+            <p className="place-rating-bold">{place.rating}</p>
+            <StarIcon className="place-rating-icon" />
+            <p className="place-rating">({place.num_reviews})</p>
+          </div>
+          {/* <MapPinIcon className="map-pin-icon"/> */}
+        </>
+      ) : null}
     </>
   );
 };
